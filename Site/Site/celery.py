03c4aee2fc9ac1567eb7dc0413bc09a1c472bb7b
timofey <ale3jurtaev@gmail.com>
2025-02-10 02:27:02 +0300

    Fix bugs

--- Site/Site/celery.py
+++ Site/Site/celery.py
@@ -21,6 +21,6 @@ app.conf.beat_schedule = {
     },
     'add_olympiads': {
         'task': 'olympic.tasks.add_olympiads',
-        'schedule': crontab(minute='*/2')
+        'schedule': crontab(hour='*/60')
     },
 }
