2061209160c6c75ef0af713b97e199a956a9bd60
timofey <ale3jurtaev@gmail.com>
2025-02-11 21:21:51 +0300

    Refactoring beat and worker

--- Site/Site/celery.py
+++ Site/Site/celery.py
@@ -9,6 +9,7 @@ app = Celery('Site')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
 
+app.conf.broker_url = 'redis://redis:6379/0'
 
 app.conf.beat_schedule = {
     'send-notification': {
