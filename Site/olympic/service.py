9f3b87a15e0f9906c95f7e5009059a66b4c7bdc9
timofey <ale3jurtaev@gmail.com>
2025-02-15 20:13:51 +0300

    Refactor DockerFile

--- Site/olympic/service.py
+++ Site/olympic/service.py
@@ -44,7 +44,7 @@ def add_olympiads_to_bd():
     chrome_options.add_argument("--disable-extensions")  # Отключение расширений
     chrome_options.add_argument("--window-size=1920,1080")  # Установка размера окна (важно для некоторых сайтов)
 
-    service = ChromeService(ChromeDriverManager().install())
+    service = ChromeService(ChromeDriverManager().install(), log_path="/home/app/chromedriver.log")
     driver = webdriver.Chrome(service=service, options=chrome_options)
 
     time.sleep(1)
